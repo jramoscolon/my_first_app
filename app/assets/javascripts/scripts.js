b2550590cfa63da73cc77c10aaa80add9fa2f834
Jose Ramos <jramoscolon@yahoo.com>
2016-01-25 16:41:49 -0400

    Added review system

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,5 +2,13 @@ $(function () {
 
 	$("div.alert").delay(1500).fadeOut("slow");
 
+	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets/images',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+
 
 });
