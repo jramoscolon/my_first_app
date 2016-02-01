90112de6f7c4fe69753e6775134007ee6a5bda6f
Jose Ramos <jramoscolon@yahoo.com>
2016-02-01 15:54:12 -0400

    fixed pagination style

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,8 +2,8 @@ $(function () {
 
 	$("div.alert").delay(1500).fadeOut("slow");
 
-	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets/images',
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
