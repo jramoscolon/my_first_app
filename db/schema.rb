36a3b068ffefdb2f6a95d884f72b2cb5df7aef08
Jose Ramos <jramoscolon@yahoo.com>
2016-02-24 16:03:41 -0400

    fixed price data-type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203173658) do
+ActiveRecord::Schema.define(version: 20160224192738) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20160203173658) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.string   "price"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
