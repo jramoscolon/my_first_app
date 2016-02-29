7619a6e1394325446a82ab2ebd59816746be3aa5
Jose Ramos <jramoscolon@yahoo.com>
2016-02-29 13:47:43 -0400

    added payments controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  post 'payments/create'
+  get 'payments/create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
